8b63a4938c63947c1a4a05b6bf1c5e4f8837bad8
danielramirez <2013.1b.cubdaniel@gmail.com>
2025-09-30 19:34:21 +0200

    conexion con bbdd exitosamente

--- conn.py
+++ conn.py
@@ -1,2 +1,12 @@
 ## conexion  con base de datos
 
+import mysql.connector
+mydb = mysql.connector.connect(
+host="localhost",
+user="root",
+port = 3307,
+password="1234",
+database="gestioncarrera"
+)
+
+print(mydb)
