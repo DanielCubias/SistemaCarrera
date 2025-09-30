d0fca0a2b60dabfff799e67d7ef631de65dc68ea
danielramirez <2013.1b.cubdaniel@gmail.com>
2025-09-30 20:27:22 +0200

    archivos creados delte, insert, select, update

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import conn as cn
 
 
 def selecionmenu():
+
     print("-----Bienvenidos al sistema de carrera-----")
     print("escoge una opcion:\n 0 - comprobar conexion\n 1 - añadir  \n 2 - actualizar \n 3 - ver \n 4 - borrar")
     numeroUsuario = int(input(" Selecciona un numero del 1 al 4 "))
 
-    while numeroUsuario < 0 or numeroUsuario >= 4:
+    if numeroUsuario == 0:
+        cn.connexion()
+    # elif numeroUsuario == 1:
 
-        if numeroUsuario == 1:
-            cn.connexion()
             
-
 
 
 selecionmenu()
