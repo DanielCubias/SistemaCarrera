857f0d825b66d20b86d888cb6c9172ad16439cef
danielramirez <2013.1b.cubdaniel@gmail.com>
2025-09-30 20:20:57 +0200

    Creando el menu para llamar a los diferentes archivos

--- conn.py
+++ conn.py
@@ -1,12 +1,14 @@
 ## conexion  con base de datos
 
-import mysql.connector
-mydb = mysql.connector.connect(
-host="localhost",
-user="root",
-port = 3307,
-password="1234",
-database="gestioncarrera"
-)
 
-print(mydb)
+def connexion():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    port = 3307,
+    password="1234",
+    database="gestioncarrera"
+    )
+
+    print("Conexion exitosamente")
